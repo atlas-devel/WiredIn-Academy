91e56296682fbc7786871cf160a032d48f4710f0
Leon <irakaramale@gmail.com>
2025-12-20 11:05:03 +0200

    chepter 2 confirmation test added

---
+++ chapter 2 test/two_dimension.py
@@ -0,0 +1,9 @@
+array = [
+    ["January", "February", "March", "April"],
+    ["May", "June", "July", "August"],
+    ["September", "October", "November", "December"],
+]
+
+for x in array:
+    for month in x:
+        print(month)
